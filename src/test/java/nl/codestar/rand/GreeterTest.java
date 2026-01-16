f091a2ada0205c27e6a4d7a956a03c6ef15117ac
JeanMarc van Leerdam <jean-marc.vanleerdam@soprasteria.com>
2026-01-16 11:15:36 +0100

    Remove warnings

--- src/test/java/nl/codestar/rand/GreeterTest.java
+++ src/test/java/nl/codestar/rand/GreeterTest.java
@@ -1,17 +1,14 @@
 package nl.codestar.rand;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class GreeterTest {
 
-    private Greeter greeter = new Greeter();
+    private final Greeter greeter = new Greeter();
 
     @Test
     public void greeterSaysHello() {
-        assertThat(greeter.sayHi(), containsString("Hello"));
+        assert(greeter.sayHi().contains("Hello"));
     }
 
 }
